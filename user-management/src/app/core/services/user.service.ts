49e0b845af640576a86a51ee3ba06bcbcbc58a2c
Asmahan EL MHASSANI <83731610+Asmahan355@users.noreply.github.com>
2026-02-27 16:17:31 +0000

    Refactor du UserService : suppression de localStorage et intégration complète de l’API REST via HttpClient

--- user-management/src/app/core/services/user.service.ts
+++ user-management/src/app/core/services/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { User } from '../models/user.model';
 import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 
 /*
@@ -29,81 +30,26 @@ import { Observable, of } from 'rxjs';
 })
 export class UserService {
 
-  private storageKey = 'users';
+  private apiUrl = 'https://jsonplaceholder.typicode.com/users';
 
-  constructor() {
-    // Si on est dans le navigateur
-    if (typeof window !== 'undefined') {
-
-      // Si aucun utilisateur n'est encore stocké
-      if (!localStorage.getItem(this.storageKey)) {
-        const users: User[] = [
-          { id: 1, name: 'Alice', email: 'alice@test.com' },
-          { id: 2, name: 'Bob', email: 'bob@test.com' }
-        ];
-
-        localStorage.setItem(this.storageKey, JSON.stringify(users));
-      }
-    }
-  }
+  constructor(private http: HttpClient) {}
 
   getUsers(): Observable<User[]> {
-    if (typeof window === 'undefined') {
-      return of([]);
-    }
-
-    const users=JSON.parse(localStorage.getItem(this.storageKey) || '[]');
-    return of(users);
+    return this.http.get<User[]>(this.apiUrl);
   }
 
-  addUser(user: User): void {
-    if (typeof window === 'undefined') {
-      return;
+  addUser(user: User): Observable<User> {
+      return this.http.post<User>(this.apiUrl,user);
     }
 
-    this.getUsers().subscribe(users=>{
-      users.push(user);
-      localStorage.setItem(this.storageKey, JSON.stringify(users));
-    });
-    
+  deleteUser(id: number): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${id}`);
   }
-
-  deleteUser(id: number): void {
-    if (typeof window === 'undefined') {
-      return;
-    }
-
-    const users = this.getUsers().subscribe(users=>{
-    const updateUsers=users.filter(u => u.id !== id);
-    localStorage.setItem(this.storageKey, JSON.stringify(updateUsers));
-    });
+  getUserById(id: number): Observable<User> {
+    return this.http.get<User>(`${this.apiUrl}/${id}`);
   }
-getUserById(id: number): Observable<User | undefined> {
-
-  return new Observable(observer => {
-
-    this.getUsers().subscribe(users => {
-      const user = users.find(u => u.id === id);
-      observer.next(user);
-      observer.complete();
-    });
-
-  });
+  updateUser(id: number, data: { name: string; email: any }): Observable<User> {
+    return this.http.put<User>(`${this.apiUrl}/${id}`,data);
+  }
 }
-updateUser(id: number, data: { name: string; email: any }): void {
 
-  this.getUsers().subscribe(users => {
-
-    for (let i = 0; i < users.length; i++) {
-      if (users[i].id === id) {
-        users[i].name = data.name;
-        users[i].email = data.email;
-        break;
-      }
-    }
-
-    localStorage.setItem(this.storageKey, JSON.stringify(users));
-
-  });
-}
-}
