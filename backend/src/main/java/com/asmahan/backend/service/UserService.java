2cacd567d9418df0aa817b8f092de076ea096a4f
Asmahan EL MHASSANI <83731610+Asmahan355@users.noreply.github.com>
2026-03-22 23:53:01 +0000

    Ajout du service UserService avec les opérations CRUD pour la gestion des utilisateurs

---
+++ backend/src/main/java/com/asmahan/backend/service/UserService.java
@@ -0,0 +1,36 @@
+package com.asmahan.backend.service;
+import com.asmahan.backend.model.User;
+import com.asmahan.backend.repository.UserRepository;
+import org.springframework.stereotype.service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userepository;
+    public UserService(UserRepository userepository){
+        this.userepository=userepository;
+    }
+
+    public List<User> getAllUsers(){
+        return userepository.findAll();
+    }
+
+    public Optional<User> getUserById(Long id){
+        return userepository.findById(id);
+    }
+
+    public User creatUser(User user){
+        return userepository.save(user);
+    }
+    public User updateUser(Long id, User user){
+        user.setId(id);
+        return userepository.save(user);
+    }
+    public void deleteUser(Long id){
+        userepository.deleteById(id);
+    }
+
+}
+ 
