0ee281f8fa47f24c90495bedf1f329f3cbd9b8d1
Asmahan EL MHASSANI <83731610+Asmahan355@users.noreply.github.com>
2026-03-31 14:54:33 +0000

    Ajout du contrôleur UserController

---
+++ backend/src/main/java/com/asmahan/backend/controller/UserController.java
@@ -0,0 +1,45 @@
+package com.asmahan.backend.controller;
+
+import com.asmahan.backend.service.UserService;
+import com.asmahan.backend.model.User;
+
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api/users")
+// Autorise Angular (port 4200) à appeler le backend
+@CrossOrigin(origins="http://localhost:4200")
+public class UserController{
+    private final UserService userService;
+
+    public UserController(UserService userService){
+        this.userService=userService;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers(){
+        return this.userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id){
+        return this.userService.getUserById(id);
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user){
+        return this.userService.creatUser(user);
+    }
+
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable Long id,@RequestBody User user){
+        return this.userService.updateUser(id, user);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id){
+        this.userService.deleteUser(id);
+    }
+}
